dbca50511df99720a7e444cb22c229957284c7a1
Eren Kulaksiz <44942737+erenkulaksiz@users.noreply.github.com>
2022-02-05 14:08:24 +0300

    cookie fix

--- notal/src/components/acceptCookies/index.js
+++ notal/src/components/acceptCookies/index.js
@@ -6,7 +6,7 @@ const AcceptCookies = ({ visible, onAccept, style }) => {
     return (
         <div style={{ position: "sticky", bottom: 16, left: 16, zIndex: 500, display: "flex", alignItems: "flex-start", maxWidth: 300, ...style }}>
             <Card css={{ width: "auto" }}>
-                <Text>We use cookies to authenticate, analyze traffic and show personalized content. By clicking "Accept Cookies" or continuing using this website you accept our use of cookies.</Text>
+                <Text>{'We use cookies to authenticate, analyze traffic and show personalized content. By clicking "Accept Cookies" or continuing using this website you accept our use of cookies.'}</Text>
                 <Button size="md" css={{ mt: 8 }} onClick={onAccept}>Accept Cookies</Button>
             </Card>
         </div>
