30b4e9d928da4714bd46b7857d16d7d77b784db3
Eren Kulaksiz <erenkulaksz@gmail.com>
2022-12-27 23:06:07 +0300

    [feat] 2.2.8

--- notal-ts/src/pages/w/[id].tsx
+++ notal-ts/src/pages/w/[id].tsx
@@ -9,6 +9,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     const { res } = ctx;
     const query = Array.isArray(ctx.query.id) ? ctx.query.id[0] : ctx.query.id;
     res.writeHead(301, { Location: `/workspace/${query}` });
-    return res.end();
+    res.end();
   }
+  return { props: {} };
 }
